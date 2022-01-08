fe1acc40af59b2a358f822171ac5f5ff36dac300
purpleturtll <purpleturtll@gmail.com>
2022-01-08 16:34:38 +0100

    5 classes,docs

---
+++ README.md
@@ -0,0 +1,19 @@
+#  Broader Named Entity Identification and Linking
+
+Ontology classes recognized by script are:
+- dbo:Building
+- dbo:Animal
+- dbo:Person
+- dbo:University
+- dbo:Organisation
+
+When no classes are avaliable for a token on dbpedia, the class "http://localhost/Other" is assigned.
+
+### Podział prac
+
+|        Osoba      |                   Zadanie                 |       Wykonane     |
+|-------------------|-------------------------------------------|--------------------|
+|Anna Ciszak        |Wczytanie tekstu źródłowego                | :heavy_check_mark: |
+|Anna Ciszak        |Podzielenie tesktu na słowa kluczowe       | :heavy_check_mark: |
+|Bartosz Ciesielczyk|Utworzenie zapytania SPARQL                | :heavy_check_mark: |
+|Bartosz Ciesielczyk|Generowanie "trójek" XML na podstawie grafu| :heavy_check_mark: |
--- main.py
+++ main.py
@@ -1,52 +1,55 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
-from pprint import pprint
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import PorterStemmer
-import string
-import rdflib
-nltk.download("stopwords")
-nltk.download("punkt")
-stop_words = set(stopwords.words("english"))
-table = str.maketrans('', '', string.punctuation)
+def main(file_name):
+    from SPARQLWrapper import SPARQLWrapper, JSON
+    from pprint import pprint
+    import nltk
+    from nltk import word_tokenize
+    from nltk.corpus import stopwords
+    from nltk.tokenize import word_tokenize
+    from nltk.stem.porter import PorterStemmer
+    import string
+    import rdflib
+    nltk.download("stopwords")
+    nltk.download("punkt")
+    stop_words = set(stopwords.words("english"))
+    table = str.maketrans('', '', string.punctuation)
 
-text = "We left in pretty good time, and came after nightfall to Klausenburgh. Here I stopped for the night at the Hotel Royale. I had for dinner, or rather supper, a chicken done up some way with red pepper, which was very good but thirsty Horse."
+    i = open(file_name, "r")
+    text = i.read()
+    text = word_tokenize(text)
+    text = [w.translate(table) for w in text]
+    text = [word for word in text if word.isalpha()]
+    text = [w for w in text if not w in stop_words]
+    text = [w.capitalize() for w in text]
+    pprint(text)
 
-text = word_tokenize(text)
-text = [w.translate(table) for w in text]
-text = [word for word in text if word.isalpha()]
-text = [w for w in text if not w in stop_words]
-text = [w.capitalize() for w in text]
-pprint(text)
+    triples = []
 
-result = []
+    for word in text:
+        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+        sparql.setQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        SELECT DISTINCT ?obj WHERE{""" +
+                        f"<http://dbpedia.org/resource/{word}>  rdf:type ?obj " +
+                        "FILTER (strstarts(str(?obj), str(dbo:))) " +
+                        "FILTER regex(?obj, \"(Building|Animal|Person|University|Organisation)$\")}")
 
-triples = []
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        if results['results']['bindings']:
+            triples.extend([(rdflib.term.URIRef(f"http://dbpedia.org/resource/{word}"), rdflib.term.URIRef(
+                "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), rdflib.term.URIRef(v['obj']['value'])) for v in results['results']['bindings']])
+        else:
+            triples.extend([(rdflib.term.URIRef(f"http://dbpedia.org/resource/{word}"), rdflib.term.URIRef(
+                "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), rdflib.term.URIRef("http://localhost/Other"))])
 
-for word in text:
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    sparql.setQuery("""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX dbr: <http://dbpedia.org/resource>
-PREFIX dbo: <http://dbpedia.org/ontology>
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?obj WHERE{""" +
-f"<http://dbpedia.org/resource/{word}>  rdf:type ?obj " +
-"FILTER strstarts(str(?obj), str(dbo:))}")
+    g = rdflib.Graph()
+    g.bind("dbr", rdflib.namespace.Namespace("http://dbpedia.org/resource/"))
+    g.bind("dbo", rdflib.namespace.Namespace("http://dbpedia.org/ontology/"))
 
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-    triples.extend([(rdflib.term.URIRef(f"http://dbpedia.org/resource/{word}"), rdflib.term.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), rdflib.term.URIRef(v['obj']['value'])) for v in results['results']['bindings']])
+    for t in triples:
+        g.add(t)
 
-g = rdflib.Graph()
-g.bind("dbr", rdflib.namespace.Namespace("http://dbpedia.org/resource/"))
-g.bind("dbo", rdflib.namespace.Namespace("http://dbpedia.org/ontology/"))
+    pprint(g.serialize(destination="triples.xlm", format="xml"))
 
-for t in triples:
-    g.add(t)
 
-print(g.serialize(destination="triples.xlm", format="xml"))
+if __name__ == "__main__":
+    main("test.txt")
